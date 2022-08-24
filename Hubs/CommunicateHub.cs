847856ae395aa1fcd24f99ece32e9e49f02065e5
rasha <=>
2022-08-25 01:14:30 +0300

    Add project files.

---
+++ Hubs/CommunicateHub.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ScalaxServer.Hubs;
+
+public class CommunicateHub : Hub
+{
+
+    private static readonly string activeConsDataPath = Environment.CurrentDirectory + "/active_cons.txt";
+    //private static readonly string activeConsData = File.ReadAllText(activeConsDataPath);
+
+
+    public Task Init(string clientId, string conId, bool isAdmin)
+    {
+
+        string prevConsData = File.ReadAllText(activeConsDataPath);
+        File.WriteAllText(activeConsDataPath, prevConsData + clientId + ':' + (isAdmin ? $"%{conId}%" : conId) + ';');
+
+        return Task.CompletedTask;
+    }
+
+
+
+
+    // # ADMIN ONLY!
+
+    public Task FetchActiveCons(string conId)
+    {
+        Console.Write("\n\n FETCHED DATA. \n\n");
+        //string adminId = (File.ReadAllText(activeConsDataPath).Split(';').ToList().Find(conId => conId.Contains("-a-"))).Replace("-a-", "");
+        string txt = File.ReadAllText(activeConsDataPath.Replace(";", "\n"));
+        return Clients.Client(conId).SendAsync("GetActiveCons", txt);
+    }
+
+    public Task ExecCmdOnOne (string targetConId, string cmd)
+    {
+        Console.WriteLine("\n\n Sent command \n\n");
+        return Clients.Client(targetConId).SendAsync("ExecCmdOnOne", cmd);
+    }
+
+    ////
+    ///
+
+
+
+    public Task SendExecCmdOnOneRes (string conId, string cmdResOutput)
+    {
+        Console.Write($"\n\n {cmdResOutput} \n\n");
+
+        return Clients.Client(File.ReadAllText(activeConsDataPath).Split(';').ToList().Find(id => id.Contains('%')).Split(':').ElementAt(1).Replace("%", String.Empty)).SendAsync("GetExecCmdOnOneRes", $"{conId} :: \n {cmdResOutput} \n\n");
+    }
+
+
+
+    //public Task SendCmd(string sender, string cmd)
+    //{
+    //    return Clients.All.SendAsync("GetCmd", cmd);
+    //}
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+
+using ScalaxServer.Hubs;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" }    
+    );
+
+});
+
+builder.Services.AddSignalR();
+
+
+var app = builder.Build();
+
+
+//app.UseDefaultFiles();
+//app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(eps =>
+{
+    eps.MapHub<CommunicateHub>("/Hubs/Communicate");
+});
+
+//app.MapBlazorHub();
+//app.MapHub<CommunicateHub>("/Hubs/Communicate");
+//app.MapFallbackToPage("/_Host");
+
+
+app.Run();
