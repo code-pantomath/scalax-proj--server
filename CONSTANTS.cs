8e3cd02f7ab3732889e377060294dbacc95be41c
vanDev <111028309+vanDev0@users.noreply.github.com>
2022-10-08 16:56:01 +0300

    Update CONSTANTS.cs

--- CONSTANTS.cs
+++ CONSTANTS.cs
@@ -11,7 +11,7 @@ namespace Scalax_server
         public static readonly string SERVER_ENDPOINT_URL = "https://scalax-server-01.herokuapp.com";
 
 
-        public static readonly string PROJECT_DIR_PATH = "app/heroku_output/";
+        public static readonly string PROJECT_DIR_PATH = "app/heroku_output/Scalax_server";
         public static readonly string WWWROOT_DIR_PATH = Path.Combine(PROJECT_DIR_PATH, "wwwroot");
         public static readonly string ACTIVE_CONS_TXTFILE_PATH = Path.Combine(PROJECT_DIR_PATH, "active_cons.txt");
         public static readonly string ACTIVE_TOKENS_TXTFILE_PATH = Path.Combine(PROJECT_DIR_PATH, "active_tks.txt");
