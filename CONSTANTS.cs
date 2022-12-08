a404dc29e3d8b343633e325069479a1bc89b90f8
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-09 02:42:08 +0300

    Update CONSTANTS.cs

--- CONSTANTS.cs
+++ CONSTANTS.cs
@@ -8,14 +8,12 @@ namespace Scalax_server
 {
     internal class CONSTANTS
     {
-        public static readonly string SERVER_ENDPOINT_URL = "https://site--scalax-server-01--n5gfpf86nbvs.code.run";
 
-
-        public static readonly string PROJECT_DIR_PATH = "/app/heroku_output";
+        
+        public static readonly string PROJECT_DIR_PATH = "/app/_output";
         public static readonly string WWWROOT_DIR_PATH = "/app/wwwroot/";
         public static readonly string ACTIVE_CONS_TXTFILE_PATH = "/app/active_cons.txt";
         public static readonly string ACTIVE_TOKENS_TXTFILE_PATH = "/app/active_tks.txt";
         
-
     }
 }
